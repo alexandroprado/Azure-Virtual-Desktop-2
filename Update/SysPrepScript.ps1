1dc3375762201524235c8b5194a991c35c6b363c
Knowledgebase <71754369+knowledgebaseit@users.noreply.github.com>
2022-01-12 15:53:02 +0100

    Update SysPrepScript.ps1

--- Update/SysPrepScript.ps1
+++ Update/SysPrepScript.ps1
@@ -1,2 +1,1 @@
-
-Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /quit /quiet'  -Wait
+Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/oobe /generalize /quit /mode:vm'  -Wait
